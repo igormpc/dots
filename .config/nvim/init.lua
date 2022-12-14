c0aefc1f8df5e4ed9d97dd4750f676da91764b3b
igor medeiros <igormpc@gmail.com>
2022-12-14 14:50:35 -0300

    Add files via upload

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+require('colorscheme')
+
+require('keymaps')
+require('lualine_config')
+require('luasnip_config')
+require('nvim-cmp')
+require("nvim-tree_config")
+require("nvim-treesitter-config")
+require('options')
+require('plugins')
+require('scnvim_config')
+require('tidal')
+
+vim.cmd [[ let g:instant_username = "igor" ]]
