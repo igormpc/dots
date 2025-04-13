e949bc26159c824812ba363663c7aae1725a4523
igor medeiros <igormpc@gmail.com>
2025-04-13 12:19:59 -0300

    Create init.el

---
+++ .config/emacs/init.el
@@ -0,0 +1,10 @@
+(add-to-list 'load-path "/usr/share/SuperCollider/Extensions/scide_scel")
+(require 'sclang)
+
+(add-to-list 'load-path "~/sc/tidal")
+(require 'haskell-mode)
+(require 'tidal)
+
+(custom-set-variables
+ '(custom-enabled-themes '(modus-vivendi)))
+(custom-set-faces)
