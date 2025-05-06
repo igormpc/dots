98bdc9314dc65b0de30b85f086243cfb669a97cc
igor medeiros <igormpc@gmail.com>
2025-05-06 09:47:14 -0300

    Create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+require('config.lazy')
+require('config.options')
+require('config.keymaps')
+
+-- vim.cmd('colorscheme hemisu')
+
+-- vim.cmd [[let g:tidal_sc_enable = 1]]
+-- vim.cmd [[let g:tidal_sc_boot = "~/.config/SuperCollider/tidal-startup.scd"]]
+-- vim.cmd [[let g:tidal_boot = "/usr/share/haskell-tidal/BootTidal.hs"]]
